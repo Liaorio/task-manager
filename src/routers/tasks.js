3eb24711b0357ec9ddf9e6f3ebcf9f8989dceee6
liao <liaorio@gmail.com>
2020-08-01 15:31:09 -0400

    data sorting, pagination and filtering

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -17,10 +17,28 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
+// tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req.query.completed) match.completed = req.query.completed === 'true';
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
     // const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate('tasks').execPopulate();  // connection is built in user model: virtual
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate();  // connection is built in user model: virtual
     // res.send(tasks);
     res.send(req.user.tasks);
   } catch(e) {
